23515a730aafc6ce4632f44b55ae85eb961c08c7
Karina Smardzewski <140887892+Carinia1@users.noreply.github.com>
2023-11-16 14:39:02 +0100

    Add files via upload

---
+++ Main01.java
@@ -0,0 +1,15 @@
+package pl.coderslab.bvariablesandoperators;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+
+		int int1 = 17;
+		char char2 = 'c';
+		long long3 = 12345678;
+		double double4 = 17.112023;
+		boolean boolean5 = false;
+
+	}
+
+}
